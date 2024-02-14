bdae43f709932eaa6c764f9ae1f79016ef990c58
Kelveen Fabian <43725042+kelveenfabian@users.noreply.github.com>
2024-02-13 23:29:52 -0500

    [WM-1] Create WesternHoroscopeResponse.kt

---
+++ app/src/main/java/com/v/waltersmarket/data/WesternHoroscopeResponse.kt
@@ -0,0 +1,40 @@
+package com.v.waltersmarket.data
+
+data class WesternHoroscopeResponse(
+    val planets: List<Planet>?,
+    val houses: List<House>?,
+    val ascendant: Double?,
+    val midHeaven: Double?,
+    val vertex: Double?,
+    val lilith: Lilith,
+    val aspects: List<Aspect>
+)
+
+data class Planet(
+    val name: String?,
+    val fullDegree: Double?,
+    val normDegree: Double?,
+    val speed: Double?,
+    val isRetro: Boolean,
+    val signId: Int?,
+    val sign: String?,
+    val house: Int?,
+)
+
+data class House(
+    val house: Int?,
+    val sign: String?,
+    val degree: Double?,
+)
+
+data class Lilith(val lilith: Planet)
+
+data class Aspect(
+    val aspectingPlanet: String?,
+    val aspectedPlanet: String?,
+    val aspectingPlanetId: Int?,
+    val aspectedPlanetId: Int?,
+    val type: String?,
+    val orb: Double?,
+    val diff: Double?,
+)
