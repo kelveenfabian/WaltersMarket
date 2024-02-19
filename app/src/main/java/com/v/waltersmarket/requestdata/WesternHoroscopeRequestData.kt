29d2b286454b230da4c00b380146ddc25beddb67
Kelveen Fabian <43725042+kelveenfabian@users.noreply.github.com>
2024-02-19 18:16:25 -0500

    [WM-1] Create WesternHoroscopeRequestData.kt

---
+++ app/src/main/java/com/v/waltersmarket/requestdata/WesternHoroscopeRequestData.kt
@@ -0,0 +1,12 @@
+package com.v.waltersmarket.requestdata
+
+data class WesternHoroscopeRequestData(
+    val day: String,
+    val month: String,
+    val year: String,
+    val hour: String,
+    val min: String,
+    val lat: String,
+    val lon: String,
+    val tzone: String,
+)
