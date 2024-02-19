62b35598201723993130a405388fd458a26c041f
Kelveen Fabian <43725042+kelveenfabian@users.noreply.github.com>
2024-02-19 18:16:37 -0500

    [WM-1] Create Planet model class

---
+++ app/src/main/java/com/v/waltersmarket/ui/model/Planet.kt
@@ -0,0 +1,12 @@
+package com.v.waltersmarket.ui.model
+
+data class Planet (
+    val name: String?,
+    val fullDegree: Double?,
+    val normDegree: Double?,
+    val speed: Double?,
+    val isRetro: Boolean,
+    val signId: Int?,
+    val sign: String?,
+    val house: Int?,
+)
