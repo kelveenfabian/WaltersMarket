c865b0a9482fbd5dfb7ec105031e6802edec3aae
Kelveen Fabian <43725042+kelveenfabian@users.noreply.github.com>
2024-02-25 22:30:38 -0500

    [WM-2] Add Authentication interface, so that we can abstract the authentication in case it needs to be changed in the future.

--- app/src/main/java/com/v/waltersmarket/data/authmanager/AstrologyAuthenticationManager.kt
+++ app/src/main/java/com/v/waltersmarket/data/authmanager/AstrologyAuthenticationManager.kt
@@ -2,9 +2,13 @@ package com.v.waltersmarket.data.authmanager
 
 import okhttp3.Credentials
 
-object AstrologyAuthenticationManager {
+object AstrologyAuthenticationManager : Authentication {
     private const val username = "628365"
     private const val password = "7f090bd0eb27540b9a65dd18aca41609"
 
-    fun getCredentials() : String = Credentials.basic(username, password)
+    override fun getCredentials() : String = Credentials.basic(username, password)
+}
+
+interface Authentication {
+    fun getCredentials(): String
 }
