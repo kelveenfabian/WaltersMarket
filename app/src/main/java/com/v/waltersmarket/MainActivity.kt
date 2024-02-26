bea045b580504bf189bf591e624c09e12289d74a
Kelveen Fabian <43725042+kelveenfabian@users.noreply.github.com>
2024-02-25 22:48:06 -0500

    [WM-2] Add TextField for Day input

--- app/src/main/java/com/v/waltersmarket/MainActivity.kt
+++ app/src/main/java/com/v/waltersmarket/MainActivity.kt
@@ -5,10 +5,16 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -31,9 +37,10 @@ class MainActivity : ComponentActivity() {
         min = "50",
     )
 
+    @ExperimentalMaterial3Api
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        viewModel.fetchWesternHoroscopeData(requestData)
+        fetchData()
         setContent {
             WaltersMarketTheme {
                 val data = viewModel.planets.collectAsStateWithLifecycle()
@@ -45,6 +52,10 @@ class MainActivity : ComponentActivity() {
                    Column(
                        modifier = Modifier.fillMaxSize()
                    ) {
+                       var text by remember { mutableStateOf("") }
+
+                       TextField(value = text, onValueChange = { text = it }, label = { Text("Day") }, maxLines = 1)
+
                        data.value.forEach {
                            Text(text = it.name!!)
                        }
@@ -54,12 +65,10 @@ class MainActivity : ComponentActivity() {
         }
     }
 
-    private fun subscribeToObservables() {
-
+    private fun fetchData() {
+        viewModel.fetchWesternHoroscopeData(requestData)
     }
 }
-
-
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
