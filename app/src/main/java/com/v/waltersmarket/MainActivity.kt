66d4f438830fe9d7db313a358c8a82e60b459e30
Kelveen Fabian <43725042+kelveenfabian@users.noreply.github.com>
2024-02-26 10:33:38 -0500

    [WM-2] Add TextField for all required request data inputs

--- app/src/main/java/com/v/waltersmarket/MainActivity.kt
+++ app/src/main/java/com/v/waltersmarket/MainActivity.kt
@@ -3,6 +3,7 @@ package com.v.waltersmarket
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -15,6 +16,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -49,12 +51,30 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    var dayText by remember { mutableStateOf("") }
+                    var monthText by remember { mutableStateOf("") }
+                    var yearText by remember { mutableStateOf("") }
+                    var lonText by remember { mutableStateOf("") }
+                    var latText by remember { mutableStateOf("") }
+                    var hourText by remember { mutableStateOf("") }
+                    var minText by remember { mutableStateOf("") }
+                    var timeZoneText by remember { mutableStateOf("") }
                    Column(
                        modifier = Modifier.fillMaxSize()
                    ) {
-                       var text by remember { mutableStateOf("") }
+                       Box(modifier = Modifier.align(Alignment.CenterHorizontally)) {
+                           Column {
+                               TextField(value = dayText, onValueChange = { dayText = it }, label = { Text("Day") }, maxLines = 1)
+                               TextField(value = monthText, onValueChange = { monthText = it }, label = { Text("Month") }, maxLines = 1)
+                               TextField(value = yearText, onValueChange = { yearText = it }, label = { Text("Year") }, maxLines = 1)
+                               TextField(value = hourText, onValueChange = { hourText = it }, label = { Text("Hour") }, maxLines = 1)
+                               TextField(value = minText, onValueChange = { minText = it }, label = { Text("Min") }, maxLines = 1)
+                               TextField(value = lonText, onValueChange = { lonText = it }, label = { Text("Longitude") }, maxLines = 1)
+                               TextField(value = latText, onValueChange = { latText = it }, label = { Text("Latitude") }, maxLines = 1)
+                               TextField(value = timeZoneText, onValueChange = { timeZoneText = it }, label = { Text("TimeZone") }, maxLines = 1)
+                           }
+                       }
 
-                       TextField(value = text, onValueChange = { text = it }, label = { Text("Day") }, maxLines = 1)
 
                        data.value.forEach {
                            Text(text = it.name!!)
