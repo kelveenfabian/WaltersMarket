b0dfbc66bb7af61aaf9780cdf731d5233080e161
Kelveen Fabian <43725042+kelveenfabian@users.noreply.github.com>
2024-02-27 23:38:27 -0500

    [WM-2] Remove fetchData() function and move TextFields to composable function

--- app/src/main/java/com/v/waltersmarket/MainActivity.kt
+++ app/src/main/java/com/v/waltersmarket/MainActivity.kt
@@ -15,9 +15,9 @@ import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.v.waltersmarket.ui.theme.WaltersMarketTheme
+import com.v.waltersmarket.ui.viewmodel.AstrologyViewModel
 import com.v.waltersmarket.ui.viewmodel.AstrologyViewModelFactory
 
 class MainActivity : ComponentActivity() {
@@ -26,11 +26,9 @@ class MainActivity : ComponentActivity() {
     @ExperimentalMaterial3Api
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //fetchData()
         setContent {
             WaltersMarketTheme {
-                val data = viewModel.planets.collectAsStateWithLifecycle()
-                // A surface container using the 'background' color from the theme
+                val planets = viewModel.planets.collectAsStateWithLifecycle()
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
@@ -40,21 +38,14 @@ class MainActivity : ComponentActivity() {
                    ) {
                        Box(modifier = Modifier.align(Alignment.CenterHorizontally)) {
                            Column {
-                               TextField(value = viewModel.dayText, onValueChange = { dayText -> viewModel.updateDay(dayText) }, label = { Text("Day") }, maxLines = 1)
-                               TextField(value = viewModel.monthText, onValueChange = { monthText -> viewModel.updateMonth(monthText) }, label = { Text("Month") }, maxLines = 1)
-                               TextField(value = viewModel.yearText, onValueChange = { yearText -> viewModel.updateYear(yearText) }, label = { Text("Year") }, maxLines = 1)
-                               TextField(value = viewModel.hourText, onValueChange = { hourText -> viewModel.updateHour(hourText) }, label = { Text("Hour") }, maxLines = 1)
-                               TextField(value = viewModel.minText, onValueChange = { minText -> viewModel.updateMinute(minText) }, label = { Text("Min") }, maxLines = 1)
-                               TextField(value = viewModel.lonText, onValueChange = { lonText -> viewModel.updateLongitude(lonText) }, label = { Text("Longitude") }, maxLines = 1)
-                               TextField(value = viewModel.latText, onValueChange = { latText -> viewModel.updateLatitude(latText) }, label = { Text("Latitude") }, maxLines = 1)
-                               TextField(value = viewModel.timeZoneText, onValueChange = { tzone -> viewModel.updateTimezone(tzone) }, label = { Text("TimeZone") }, maxLines = 1)
-                               Button(onClick = { viewModel.fetchWesternHoroscopeData() }, modifier = Modifier.align(Alignment.CenterHorizontally) ) {
+                               UserInput(viewModel = viewModel)
+                               Button(onClick = { viewModel.fetchWesternHoroscopeData() }, modifier = Modifier.align(Alignment.CenterHorizontally)) {
                                    Text(text = "Fetch Data")
                                }
                            }
                        }
 
-                       data.value.forEach {
+                       planets.value.forEach {
                            Text(text = it.name!!, modifier = Modifier.align(Alignment.CenterHorizontally))
                        }
                    }
@@ -62,24 +53,17 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
-
-    private fun fetchData() {
-        viewModel.fetchWesternHoroscopeData()
-    }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    WaltersMarketTheme {
-        Greeting("Android")
-    }
+fun UserInput(viewModel: AstrologyViewModel) {
+    TextField(value = viewModel.dayText, onValueChange = { dayText -> viewModel.updateDay(dayText) }, label = { Text("Day") }, maxLines = 1)
+    TextField(value = viewModel.monthText, onValueChange = { monthText -> viewModel.updateMonth(monthText) }, label = { Text("Month") }, maxLines = 1)
+    TextField(value = viewModel.yearText, onValueChange = { yearText -> viewModel.updateYear(yearText) }, label = { Text("Year") }, maxLines = 1)
+    TextField(value = viewModel.hourText, onValueChange = { hourText -> viewModel.updateHour(hourText) }, label = { Text("Hour") }, maxLines = 1)
+    TextField(value = viewModel.minText, onValueChange = { minText -> viewModel.updateMinute(minText) }, label = { Text("Min") }, maxLines = 1)
+    TextField(value = viewModel.lonText, onValueChange = { lonText -> viewModel.updateLongitude(lonText) }, label = { Text("Longitude") }, maxLines = 1)
+    TextField(value = viewModel.latText, onValueChange = { latText -> viewModel.updateLatitude(latText) }, label = { Text("Latitude") }, maxLines = 1)
+    TextField(value = viewModel.timeZoneText, onValueChange = { tzone -> viewModel.updateTimezone(tzone) }, label = { Text("TimeZone") }, maxLines = 1)
 }
